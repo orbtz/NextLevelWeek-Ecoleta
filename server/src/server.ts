53b965d59996c1e7a663ab9a2f9a05b9587bc97f
Fábio Barbosa <fab.rb@outlook.com>
2020-06-02 21:12:15 -0300

    Day 2: Routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,13 @@
 import express from "express";
+import routes from "./routes";
+import cors from 'cors';
+import path from 'path';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log("Teste da Rota /users");
-    
-    response.json([
-        'Fábio',
-        'Rafael',
-        'Barbosa'
-    ]);
-});
+app.use(cors());
+app.use(express.json()); //Para entender as requisições JSON
+app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
